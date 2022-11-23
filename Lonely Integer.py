d4e2c25f5aad9f3fa02e8650300a552dd1647eae
Zippo <digeronimovalentin@gmail.com>
2022-11-23 17:50:52 -0300

    Solved

---
+++ Lonely Integer.py
@@ -0,0 +1,16 @@
+# Given an array of integers, where all elements but one occur twice, find the unique element.
+
+# Example
+# a = [1,2,3,4,3,2,1]
+# The unique element is 4.
+
+def lonelyinteger(a):
+    for x in a:
+        if a.count(x) == 1:
+            return x
+
+
+# <--------------- Others --------------->
+
+def lonelyinteger(a):
+    return [x for x in a if a.count(x) == 1][0]
